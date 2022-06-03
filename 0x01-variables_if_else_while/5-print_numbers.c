7e344e92679703d869b340a2c98e784a7ab18365
latiiLA <amelatbay@gmail.com>
2022-06-03 06:48:41 -0700

    5-print_numbers.c is added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	num = 10;
+
+	while (num <= 10)
+	{
+		printf(num);
+		num++;
+	}	
+	return (0);
+}
