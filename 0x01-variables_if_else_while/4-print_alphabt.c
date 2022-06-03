9ddc2bd438696e370a0f93d6bd6014c3f62ee810
latiiLA <amelatbay@gmail.com>
2022-06-03 06:27:52 -0700

    updated

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,14 @@ int main(void)
 {
 	char alpha;
 
-	alpha = 'a'
+	alpha = 'a';
 	while (alpha <= 'z')
 	{
 		if(alpha != 'q' || alpha != 'e')
 		{
 			putchar(alpha);
 		}
+		alpha++;
 	}
 	putchar('\n');
 	return (0);
