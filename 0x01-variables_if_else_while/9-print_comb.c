2bff5e9c30392c561d60fcf7e69e5d9d1a86887e
latiiLA <amelatbay@gmail.com>
2022-06-03 16:56:23 -0700

    added

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,14 @@
 int main(void)
 {
 	int num;
-	int hexa;
-	
+		
 	num = 0;
-	hexa = 65;
 	while (num < 10)
 	{
 		putchar(num + '0');
 		num++;
 		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
