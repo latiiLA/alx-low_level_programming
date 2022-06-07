f9d80e4b52967d09fdeb574a781b0e06f2847980
latiiLA <amelatbay@gmail.com>
2022-06-07 12:35:47 -0700

    alphabet is added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int n;
+
+	n = 1;
+
+	while (n < 26)
+		_putchar(n);
+	_putchar('\n');
+	
+}
