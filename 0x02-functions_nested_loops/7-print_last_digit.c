fcc00c20166ac33aefd795b89e6a02f73055055b
latiiLA <amelatbay@gmail.com>
2022-06-08 12:33:30 -0700

    7-printlastdigit is added

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - check the code
+ *
+ * @num: parameter
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int num)
+{
+	_putchar(num % 10);
+	return (num % 10);
+}
