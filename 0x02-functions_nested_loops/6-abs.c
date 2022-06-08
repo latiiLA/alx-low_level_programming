56774ecfe6ddae2d2de05ae43bb533e68e3e8545
latiiLA <amelatbay@gmail.com>
2022-06-08 12:23:55 -0700

    6-abs is added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - check the code
+ *
+ * @c: parameter
+ *
+ * Return: Always 0.
+ */
+int _abs(int c)
+{
+	if (c < 0)
+		return (c * -1);
+	return (c);
+}
