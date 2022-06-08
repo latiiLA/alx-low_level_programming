97d4d17c9f5196032309a8d28d65bcafa15062ff
latiiLA <amelatbay@gmail.com>
2022-06-08 00:32:01 -0700

    modified

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,13 @@
 /**
  * _islower - checks for lowerchase character
  *
- * Return: Always 0.
+ * c - parameter for function _islower
+ *
+ * Return: Always 0 if not alphabet else return 1.
  */
 int _islower(int c)
 {
-	if ( c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
