7d1c65d4ae9f8007e9fb49f4db9ea7bee471efc5
latiiLA <amelatbay@gmail.com>
2022-06-09 14:03:44 -0700

    main.h, 3-print_numbers, 3-main.c are added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+int print_numbers(void)
+{
+	int num;
+
+	num = 0;
+
+	while (num < 10)
+	{
+		_putchar(num + 0);
+		num++;
+	}
+
+	return (0);
+}
