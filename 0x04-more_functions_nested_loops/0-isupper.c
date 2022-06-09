d4ff4345e9821e98c359caea00221934e2a78365
latiiLA <amelatbay@gmail.com>
2022-06-09 06:30:01 -0700

    isupper is added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code.
+ *
+ * @c: first parameter
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
