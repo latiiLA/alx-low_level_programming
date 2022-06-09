98bbb3dc01a12bb74354e0ab60749c445d2793d2
latiiLA <amelatbay@gmail.com>
2022-06-09 06:55:19 -0700

    is digit is added

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code
+ *
+ * @c: first parameter
+ *
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <=57)
+		return (1);
+	return (0);
+}
