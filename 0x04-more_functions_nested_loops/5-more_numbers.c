82d1affd4c6f340c0bae897257d91cbfc2496474
latiiLA <amelatbay@gmail.com>
2022-06-10 00:24:23 -0700

    changes

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int number, count;
+
+	count = 1;
+
+	while (count < 11)
+	{
+		number = 0;
+		while (number <= 14)
+		{
+			if (i >= 10)
+			{
+				_putchar((number / 10) + '0');
+			}
+			_putchar((number % 10) + '0');
+			number++;
+		}
+		_putchar('\n');
+		count++;
+	}
+}
