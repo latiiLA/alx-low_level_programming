9a6446ade105b765582dcde747d3136ce1942a6d
latiiLA <amelatbay@gmail.com>
2022-06-10 04:47:07 -0700

    chnage is made

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,12 +9,12 @@
  */
 void print_square(int size)
 {
-	int i, j;
-	
+	int i;
 	if (size <= 0)
 		_putchar('\n');
 	else
 	{
+		int j;
 		for (i = 1; i <= size; i++)
 		{
 			for (j = 1; j <= size; j++)
