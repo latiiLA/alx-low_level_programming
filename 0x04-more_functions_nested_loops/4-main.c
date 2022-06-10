981e8284e9d599e3dbca1efa87be437e79a7be31
latiiLA <amelatbay@gmail.com>
2022-06-09 23:32:15 -0700

    changes

--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -1,20 +1,12 @@
 #include "main.h"
 
 /**
- * print_most_numbers - check the code
+ * main - check the code
  *
  * Return: Always 0.
  */
-void print_most_numbers(void)
+int main(void)
 {
-	int num;
-
-	num = 0;
-
-	while (num <= 0)
-	{
-		if (num != 2 || num != 4)
-			_putchar(num + '0');
-	}
-	_putchar('\n');
+    print_most_numbers();
+    return (0);
 }
