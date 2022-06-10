0939bc92580571cc931c221a32444089abf88b7c
latiiLA <amelatbay@gmail.com>
2022-06-09 23:16:33 -0700

    4-print_most_numbers.c is added

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	num = 0;
+
+	while (num <= 0)
+	{
+		if (num != 2 || num != 4)
+			_putchar(num + '0');
+	}
+	_putchar('\n');
+}
