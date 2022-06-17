38fbcdd4a59e9fe8b34c4fccd91a4cb8c8214eee
latiiLA <amelatbay@gmail.com>
2022-06-17 12:51:47 -0700

    changes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,17 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
 
-	while (dest[j] != '\0')
-	{
-		j++;
-	}
-
 	while (src[i] != '\0' && i < (j + n))
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\n';
+	dest[i] = '\0';
 
 	return (dest);
 }
