a72fd16066662a460db1caf0089e322f2cfe03f8
latiiLA <amelatbay@gmail.com>
2022-06-22 13:10:28 -0700

    added

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * _puts_recursion - check the code
- * @s: paramenter 1
- *
- * Return: Always 0.
+ * _puts_recursion - print a string.
+ * @s: pointer to char
+ * Return: No.
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s++);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
-
 }
