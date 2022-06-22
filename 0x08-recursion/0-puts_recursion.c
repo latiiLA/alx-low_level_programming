275b501f9d967d4fdf48be0b740cf5d9033cadcf
latiiLA <amelatbay@gmail.com>
2022-06-22 12:48:54 -0700

    added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check the code
+ * @s: paramenter 1
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion (s++);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
